16e919abd7ad0b9af62a87e6a8cac7c3633013d3
mayrhiarm <mariamsanni932gmail.com>
2022-06-24 16:40:08 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,6 @@
+let Name = "Mariam Sanni";
+console.log("My Name is " + Name);
+let Height = 6.2;
+console.log("My Height is " + Height);
+let Country = "Nigeria";
+console.log("I am from " + Country);
